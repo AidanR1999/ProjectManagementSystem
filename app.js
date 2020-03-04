154fbf0059b802c74b1165c23ee9abc8a5190797
Aidan <aidanr1999@hotmail.com>
2020-03-04 14:59:13 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var mustache = require("mustache-express");
+var path = require("path");
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.engine("mustache", mustache());
+app.set("view engine", "mustache");
+app.set("views", path.resolve(__dirname, "views"));
+
+app.get('/', function (req, res) {
+    res.status(200);
+    res.type("text/html");
+    res.send("<h1> Landing Page </h1>");
+});
+
+// 404 catch-all handler (middleware)
+app.use(function (req, res, next) {
+    res.status(404); res.render('404');
+});
+// 500 error handler (middleware)
+app.use(function (err, req, res, next) {
+    console.error(err.stack); res.status(500); res.render('500.html');
+})
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') +
+        '; press Ctrl-C to terminate.');
+});
