7c88dea3c5edf5cf45362fc6d41e0387ae490272
Aidan <aidanr1999@hotmail.com>
2020-03-04 16:09:47 +0000

    installed bootsrap

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
+//require
+//====================================================================
 var express = require('express');
 var mustache = require("mustache-express");
 var path = require("path");
 
+//express
+//====================================================================
 var app = express();
-app.set('port', process.env.PORT || 3000);
 
+//configure
+//====================================================================
+app.set('port', process.env.PORT || 3000);
 app.engine("mustache", mustache());
 app.set("view engine", "mustache");
 app.set("views", path.resolve(__dirname, "views"));
 
+//routes
+//====================================================================
 app.get('/', function (req, res) {
-    res.status(200);
-    res.type("text/html");
-    res.send("<h1> Landing Page </h1>");
+    res.render('home', {})
 });
-
-// 404 catch-all handler (middleware)
-app.use(function (req, res, next) {
-    res.status(404); res.render('404');
-});
-// 500 error handler (middleware)
-app.use(function (err, req, res, next) {
-    console.error(err.stack); res.status(500); res.render('500.html');
-})
 
 app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' + app.get('port') +
