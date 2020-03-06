7e9b6219ba8143b63c533f9ea4da4739235f1981
luki110 <37291567+luki110@users.noreply.github.com>
2020-03-06 15:35:41 +0000

    database implementation

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-var express = require('express');
+var express = require("express");
 var mustache = require("mustache-express");
 var path = require("path");
+var Seed = require("./data/seed");
+let seed = new Seed();
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
-
+app.set("port", process.env.PORT || 3000);
 app.engine("mustache", mustache());
 app.set("view engine", "mustache");
 app.set("views", path.resolve(__dirname, "views"));
 
-app.get('/', function (req, res) {
-    res.status(200);
-    res.type("text/html");
-    res.send("<h1> Landing Page </h1>");
+//seed data
+seed.init();
+//seed.initProjects();
+
+
+app.get("/", function(req, res) {
+  res.status(200);
+  res.type("text/html");
+  res.send("<h1> Landing Page </h1>");
 });
 
 // 404 catch-all handler (middleware)
-app.use(function (req, res, next) {
-    res.status(404); res.render('404');
+app.use(function(req, res, next) {
+  res.status(404);
+  res.render("404");
 });
 // 500 error handler (middleware)
-app.use(function (err, req, res, next) {
-    console.error(err.stack); res.status(500); res.render('500.html');
-})
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.render("500.html");
+});
 
-app.listen(app.get('port'), function () {
-    console.log('Express started on http://localhost:' + app.get('port') +
-        '; press Ctrl-C to terminate.');
+app.listen(app.get("port"), function() {
+  console.log(
+    "Express started on http://localhost:" +
+      app.get("port") +
+      "; press Ctrl-C to terminate."
+  );
 });
