391b9c533ede783f616d06093a8b8542c18393a5
Aidan <aidanr1999@hotmail.com>
2020-05-06 13:02:12 +0100

    added get all user projects

--- data/DatabaseAO.js
+++ data/DatabaseAO.js
@@ -216,6 +216,19 @@ class DatabaseAO {
             });
         });
     }
+
+    getUserProjects(userId) {
+        return new Promise((resolve, reject) => {
+            this.Projects.find( {ownerId: userId}, (err, docs) => {
+                if(err) {
+                    reject(err);
+                    console.log("could not find project");
+                } else {
+                    resolve(docs);
+                }
+            });
+        });
+    }
     
     
 
